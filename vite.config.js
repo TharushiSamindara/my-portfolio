20b605768d757bf2f54d0af59d343666c814f53d
TharushiSamindara <tharushisamindara@gmail.com>
2025-09-04 19:43:07 +0530

    my portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
-import path from "path";
+import path from "path"
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+// Fix __dirname in ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
+  base: '/my-portfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
